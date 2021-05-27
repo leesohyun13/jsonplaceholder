9031f0a8c5fad326b1e5d759adbd801f7e0ab4bc
thgus5614@naver.com <thgus5614@naver.com>
2021-05-27 23:03:50 +0900

    Add PostsRepository

---
+++ app/src/main/java/com/sohyun/jsonplaceholder/data/repository/PostsRepository.kt
@@ -0,0 +1,13 @@
+package com.sohyun.jsonplaceholder.data.repository
+
+import com.sohyun.jsonplaceholder.data.model.Comment
+import com.sohyun.jsonplaceholder.data.model.Post
+import com.sohyun.jsonplaceholder.data.network.NetworkStatus
+
+interface PostsRepository {
+    suspend fun getPosts(start: Int): NetworkStatus<List<Post>>
+    suspend fun searchPostItem(id: Int): NetworkStatus<Post>
+    suspend fun getComments(id: Int): NetworkStatus<List<Comment>>
+    suspend fun deletePost(id: Int)
+    suspend fun updateData(id: Int, content: String)
+}
---
+++ app/src/main/java/com/sohyun/jsonplaceholder/data/repository/PostsRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.sohyun.jsonplaceholder.data.repository
+
+import com.sohyun.jsonplaceholder.data.model.Comment
+import com.sohyun.jsonplaceholder.data.model.Post
+import com.sohyun.jsonplaceholder.data.network.NetworkStatus
+import com.sohyun.jsonplaceholder.data.network.PlaceHoldApi
+import javax.inject.Inject
+
+class PostsRepositoryImpl @Inject constructor(
+    private val placeHoldApi: PlaceHoldApi
+): PostsRepository, BaseRepository(){
+    override suspend fun getPosts(start: Int): NetworkStatus<List<Post>> =
+        safeApiCall { placeHoldApi.getPosts(start) }
+
+    override suspend fun searchPostItem(id: Int): NetworkStatus<Post> =
+        safeApiCall { placeHoldApi.searchPostItem(id) }
+
+
+    override suspend fun getComments(id: Int): NetworkStatus<List<Comment>> =
+        safeApiCall { placeHoldApi.getComments(id) }
+
+    override suspend fun deletePost(id: Int) = placeHoldApi.deletePost(id)
+
+    // FIXME
+    override suspend fun updateData(id: Int, content: String) = placeHoldApi.updateData(id, content)
+}
