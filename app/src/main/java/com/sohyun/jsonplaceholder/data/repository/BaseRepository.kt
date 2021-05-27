44e199dc9acfa954872b1aae39e4d644ffadf6c3
thgus5614@naver.com <thgus5614@naver.com>
2021-05-27 23:01:03 +0900

    Add BaseRepository

---
+++ app/src/main/java/com/sohyun/jsonplaceholder/data/repository/BaseRepository.kt
@@ -0,0 +1,26 @@
+package com.sohyun.jsonplaceholder.data.repository
+
+import com.sohyun.jsonplaceholder.data.network.NetworkStatus
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+
+
+abstract class BaseRepository {
+    suspend fun <T> safeApiCall(apiCall: suspend () -> T): NetworkStatus<T> {
+        return withContext(Dispatchers.IO) {
+            try {
+                NetworkStatus.Success(apiCall.invoke())
+            } catch (throwable: Throwable) {
+                when (throwable) {
+                    is HttpException -> {
+                        NetworkStatus.Failure(isNetworkError = false, errorCode = throwable.code(), errorBody = throwable.response()?.errorBody())
+                    }
+                    else -> {
+                        NetworkStatus.Failure(true, null, null)
+                    }
+                }
+            }
+        }
+    }
+}
