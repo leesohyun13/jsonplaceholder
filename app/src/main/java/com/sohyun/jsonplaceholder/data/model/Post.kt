eafe7c27437a6e09ac27f1aa270f5398fe03877a
thgus5614@naver.com <thgus5614@naver.com>
2021-05-28 18:22:41 +0900

    Add kotlin-parcelize and Apply on Post data class

--- app/src/main/java/com/sohyun/jsonplaceholder/data/model/Post.kt
+++ app/src/main/java/com/sohyun/jsonplaceholder/data/model/Post.kt
@@ -1,7 +1,10 @@
 package com.sohyun.jsonplaceholder.data.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Post(
     @SerializedName("userId")
     val userId: Int,
@@ -11,4 +14,4 @@ data class Post(
     val title: String,
     @SerializedName("body")
     val body: String
-)
+): Parcelable
